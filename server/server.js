c42b3baaac347b74434ba04fb68282fc5e6a6dec
JoshLampen <joshlampen2@gmail.com>
2021-04-29 19:32:35 -0700

    Set postinstall script and serve static file in server

--- server/server.js
+++ server/server.js
@@ -9,6 +9,10 @@ app.use(express.static(path.join(__dirname, 'dist')));
 app.use('/*', allowCorsHandler);
 app.set('port', port);
 
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+});
+
 var server = app.listen(app.get('port'), () => {
     console.log('listening on port ', server.address().port);
 });
